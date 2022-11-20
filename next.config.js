17957180f5d62c4fda614b84b26ecd7e6b18a144
Koen Verburg <creativekoen@gmail.com>
2022-11-20 12:21:46 +0100

    chore: hooked up to domain

--- next.config.js
+++ next.config.js
@@ -1,19 +1,19 @@
 const serverSettings = {}
 
-const isGithubActions = process.env.GITHUB_ACTIONS === 'true' || false
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-  // trim off `<owner>/`
-
-  serverSettings.basePath = `/${repo}`
-  serverSettings.assetPrefix = `/${repo}/`
-
-  serverSettings.env = {
-    isGithubActions,
-    prefix: `/${repo}`,
-  }
-}
+// const isGithubActions = process.env.GITHUB_ACTIONS === 'true' || false
+//
+// if (isGithubActions) {
+//   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+//   // trim off `<owner>/`
+//
+//   serverSettings.basePath = `/${repo}`
+//   serverSettings.assetPrefix = `/${repo}/`
+//
+//   serverSettings.env = {
+//     isGithubActions,
+//     prefix: `/${repo}`,
+//   }
+// }
 
 const nextConfig = {
   reactStrictMode: true,
@@ -21,7 +21,6 @@ const nextConfig = {
 
   images: {
     unoptimized: true,
-    // loader: 'custom'
   },
 
   ...serverSettings,
