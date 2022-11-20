32237457ad877a8771f999d9d695f523420a4b02
Koen Verburg <creativekoen@gmail.com>
2022-11-20 11:33:03 +0100

    chore: used next/image instead of img

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import Link from 'next/link'
 import Layout from '../components/Layout'
+import Image from 'next/image'
 
 const links = [
   { label: 'Github',    href: 'https://github.com/koenverburg'                },
@@ -13,10 +13,12 @@ const links = [
 const IndexPage = () => (
   <Layout title="Conrad The Programmer">
     <div className="text-center mb-4">
-      <img
+      <Image
+        width="460"
+        height="460"
+        alt="Avatar"
         src="/assets/avatar-github.jpg"
         className="rounded-full w-32 mb-4 mx-auto"
-        alt="Avatar"
       />
       <h5 className="text-xl font-medium leading-tight mb-2">Conrad The Programmer</h5>
       <p className="text-gray-500">I write code and share my coding journey on these social media platforms</p>
@@ -24,7 +26,7 @@ const IndexPage = () => (
 
     <ul className="flex flex-col justify-center max-w-sm mx-auto">
       {links.map(link =>
-        <li className={`mb-4 p-1 text-center rounded-full bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500`}>
+        <li key={link.label} className={`mb-4 p-1 text-center rounded-full bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500`}>
           <a href={link.href} className={`px-4 py-2 block text-black font-semibold rounded-full bg-white`} target="_blank">
             {link.label}
           </a>
