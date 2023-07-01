da97049372198caffd8bf6562f555a3c954f6b38
Koen Verburg <creativekoen@gmail.com>
2023-07-01 10:32:30 +0200

    chore: removed umami

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,10 @@
 import * as React from 'react'
 
-import Script from 'next/script'
 import '../styles/global.scss'
 
 export default function MyApp ({ Component, pageProps }) {
   return (
     <>
-      <Script
-        async
-        defer
-        src={process.env.NEXT_PUBLIC_UMAMI_DATA_DOMAIN}
-        data-domain={process.env.NEXT_PUBLIC_UMAMI_URL}
-        data-website-id={process.env.NEXT_PUBLIC_UMAMI_SITE_ID}
-      />
       <Component {...pageProps} />
     </>
   )
